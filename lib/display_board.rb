07eabd44e2804b092953bfa233716154d2fb76ae
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-01 17:40:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(a)
+def display_board(a=[" "," "," "," "," "," "," "," "," "])
   " #{a[0]} | #{a[1]} | #{a[2]} "
   "-----------"
   " #{a[3]} | #{a[4]} | #{a[5]} "
